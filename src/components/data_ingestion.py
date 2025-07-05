e68bc331121d86b46a08b598a084974d0cdac86a
Anuj Ghogare <anujghogare6@gmail.com>
2025-07-05 12:11:02 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
                                                             ### When you use @dataclass on a class, it automatically adds: constructor , equality comparison 
 
 @dataclass                                                  
@@ -48,5 +52,8 @@ class DataIngetion:
 
 if __name__=="__main__":
     obj=DataIngetion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
